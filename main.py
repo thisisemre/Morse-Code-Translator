753b8d3c495263bc9fb870b98aa057ef77ccb976
Emre Yıldız <emreyildiz183@gmail.com>
2022-09-25 15:32:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+text = input("Write your text ").upper()
+listed_text = list(text)
+translated_list = []
+for letter in listed_text:
+    if not letter == " ":
+        translated_letter = MORSE_CODE_DICT[letter]
+        translated_list.append(translated_letter)
+
+    else:
+        translated_list.append("/")
+last_version = " ".join(translated_list)
+print(last_version)
